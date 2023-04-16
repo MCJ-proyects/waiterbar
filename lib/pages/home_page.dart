597ba5a360072ad581f5fdfd1429cfd4797cb0f1
Manuel Capilla Jiménez <manucjinf4@gmail.com>
2023-04-16 13:01:54 +0200

    Mod. Pagina de inicio para comprobar si funciona

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,36 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  final user = FirebaseAuth.instance.currentUser;
+
+  //Cerrar sesión
+  void sungUserOut() {
+    FirebaseAuth.instance.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Te has logeado!"))
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: sungUserOut, 
+            icon: const Icon(Icons.logout)
+          )
+        ],
+      ),
+      body: Center(
+        child: Text(
+          "Te has logeado como ${user!.email}",
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        )
+      )
     );
   }
 }
