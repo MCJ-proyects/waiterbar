78a350cd328e4b96af4a1efb4635397bd98d9cd2
Manuel Capilla Jiménez <manucjinf4@gmail.com>
2023-05-23 18:21:48 +0200

    Añadido transicion para el inicio de la app

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:waiterbar/pages/auth_page.dart';
@@ -19,9 +21,16 @@ class WaiterBar extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AuthPage(),
+      home: AnimatedSplashScreen(
+            duration: 2000,
+            splash: "lib/images/logo.png",
+            nextScreen: AuthPage(),
+            splashTransition: SplashTransition.fadeTransition,
+            pageTransitionType: PageTransitionType.bottomToTop,
+            backgroundColor: Colors.blue
+        ),
     );
   }
 }
