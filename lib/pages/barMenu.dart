a59444395a10560368e4d14a057be4bb10e00155
Manuel Capilla Jiménez <manucjinf4@gmail.com>
2023-06-01 22:15:09 +0200

    Añadido comentarios al codigo

--- lib/pages/barMenu.dart
+++ lib/pages/barMenu.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:waiterbar/pages/pago.dart';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
 
-
 // Creamos una clase llamada BarMenu que extiende de StatefulWidget.
 class BarMenu extends StatefulWidget {
   // Definimos un atributo final de tipo String llamado result.
@@ -36,40 +35,42 @@ class _BarMenuState extends State<BarMenu> {
 
   _BarMenuState({required this.result}); // Constructor de la clase _BarMenuState
 
-  Future<void> loadBarData() async { // Función asíncrona para cargar los datos del bar desde Firestore
-      var data = await bares.doc(result).get(); // Se obtienen los datos del documento correspondiente al bar seleccionado
-      var datos = data.data() as Map<String, dynamic>; // Se almacenan los datos en un mapa
+  // Función asíncrona para cargar los datos del bar desde Firestore
+  Future<void> loadBarData() async {
+    var data = await bares.doc(result).get(); // Se obtienen los datos del documento correspondiente al bar seleccionado
+    var datos = data.data() as Map<String, dynamic>; // Se almacenan los datos en un mapa
 
+    setState(() {
+      cuenta = 0; // Se inicializa la cuenta en cero
+    });
+
+    // Si existen tapas en los datos obtenidos
+    if (!(datos['tapas']?.isEmpty ?? true)) {
       setState(() {
-        cuenta = 0; // Se inicializa la cuenta en cero
+        tapas = datos['tapas']; // Se almacenan las tapas en la lista correspondiente
+        tapasPedidas = List<int>.filled(tapas!.length, 0); // Se inicializa la lista de tapas pedidas con ceros
       });
-
-      if (!(datos['tapas']?.isEmpty ?? true)) { // Si existen tapas en los datos obtenidos
-        setState(() {
-          tapas = datos['tapas']; // Se almacenan las tapas en la lista correspondiente
-          tapasPedidas = List<int>.filled(tapas!.length, 0); // Se inicializa la lista de tapas pedidas con ceros
-        });
-      }
-
-      if (!(datos['bebidas']?.isEmpty ?? true)) { // Si existen bebidas en los datos obtenidos
-        setState(() {
-          bebidas = datos['bebidas']; // Se almacenan las bebidas en la lista correspondiente
-          bebidasPedidas = List<int>.filled(bebidas!.length, 0); // Se inicializa la lista de bebidas pedidas con ceros
-        });
-      }
-
-      if (!(datos['postres']?.isEmpty ?? true)) { // Si existen postres en los datos obtenidos
-        setState(() {
-          postres = datos['postres']; // Se almacenan los postres en la lista correspondiente
-          postresPedidos = List<int>.filled(postres!.length, 0); // Se inicializa la lista de postres pedidos con ceros
-        });
-      }
     }
 
+    // Si existen bebidas en los datos obtenidos
+    if (!(datos['bebidas']?.isEmpty ?? true)) {
+      setState(() {
+        bebidas = datos['bebidas']; // Se almacenan las bebidas en la lista correspondiente
+        bebidasPedidas = List<int>.filled(bebidas!.length, 0); // Se inicializa la lista de bebidas pedidas con ceros
+      });
+    }
+
+    // Si existen postres en los datos obtenidos
+    if (!(datos['postres']?.isEmpty ?? true)) {
+      setState(() {
+        postres = datos['postres']; // Se almacenan los postres en la lista correspondiente
+        postresPedidos = List<int>.filled(postres!.length, 0); // Se inicializa la lista de postres pedidos con ceros
+      });
+    }
+  }
 
   // Define la función "sumar" que toma un índice y un tipo de producto como argumentos
   void sumar(int index, String tipo) {
-    
     var nombre; // Declara una variable llamada "nombre"
     num precio = 0; // Inicializa una variable "precio" con valor cero
     
@@ -144,100 +145,104 @@ class _BarMenuState extends State<BarMenu> {
     loadBarData();
   }
 
-// Define una función que construye una lista de elementos dinámicos y devuelve un widget
-Widget _buildList(List<dynamic>? items, Function(int) onTap, String emptyMessage, String title) {
-  // Si la lista de elementos está vacía, devuelve un ListTile con un mensaje vacío centrado
-  if (items?.isEmpty ?? true) {
-    return ListTile(
-      title: Text(emptyMessage, textAlign: TextAlign.center),
+  // Define una función que construye una lista de elementos dinámicos y devuelve un widget
+  Widget _buildList(List<dynamic>? items, Function(int) onTap, String emptyMessage, String title) {
+    // Si la lista de elementos está vacía, devuelve un ListTile con un mensaje vacío centrado
+    if (items?.isEmpty ?? true) {
+      return ListTile(
+        title: Text(emptyMessage, textAlign: TextAlign.center),
+      );
+    }
+
+    // De lo contrario, devuelve un ExpansionTile con el título especificado y una ListView.separated de los elementos
+    return ExpansionTile(
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.black,
+          fontStyle: FontStyle.italic,
+          fontSize: 25,
+        ),
+      ),
+      children: [
+        ListView.separated(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          itemCount: items!.length,
+          itemBuilder: (context, index) {
+            // Obtiene la referencia a la imagen del elemento en Firebase Storage
+            final ref = firebase_storage.FirebaseStorage.instance.ref(items[index]['imagenItem']);
+
+            // Devuelve un Card con información detallada del elemento
+            return Card(
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  FutureBuilder(
+                    future: ref.getDownloadURL(),
+                    builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+                      // Si se ha obtenido la URL de descarga de la imagen, muestra la imagen en un contenedor
+                      if (snapshot.hasData) {
+                        return Container(
+                          height: 225,
+                          width: 175,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: NetworkImage(snapshot.data!),
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                        );
+                      } else {
+                        // De lo contrario, muestra un indicador de progreso
+                        return const CircularProgressIndicator();
+                      }
+                    },
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 55.0, horizontal: 8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          // Muestra el nombre y precio del elemento
+                          Text(
+                            '${items[index]['nombre']} | ${items[index]['precio']}€',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 8),
+                          // Muestra la descripción del elemento
+                          Text(
+                            items[index]['descripcion'],
+                            textAlign: TextAlign.center,
+                          ),
+                          const SizedBox(height: 8),
+                          // Añade un botón para añadir el elemento al carrito
+                          ElevatedButton(
+                            onPressed: () => onTap(index),
+                            child: const Text('Añadir'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(),
+        ),
+      ],
     );
   }
 
-  // De lo contrario, devuelve un ExpansionTile con el título especificado y una ListView.separated de los elementos
-  return ExpansionTile(
-    title: Text(
-      title,
-      style: const TextStyle(
-        color: Colors.black,
-        fontStyle: FontStyle.italic,
-        fontSize: 25,
-      ),
-    ),
-    children: [
-      ListView.separated(
-        scrollDirection: Axis.vertical,
-        shrinkWrap: true,
-        itemCount: items!.length,
-        itemBuilder: (context, index) {
-          // Obtiene la referencia a la imagen del elemento en Firebase Storage
-          final ref = firebase_storage.FirebaseStorage.instance.ref(items[index]['imagenItem']);
-
-        return Card(
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FutureBuilder(
-                future: ref.getDownloadURL(),
-                builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                  if (snapshot.hasData) {
-                    return Container(
-                      height: 225,
-                      width: 175,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: NetworkImage(snapshot.data!),
-                          fit: BoxFit.fitHeight,
-                        ),
-                      ),
-                    );
-                  } else {
-                    return const CircularProgressIndicator();
-                  }
-                },
-              ),
-              const SizedBox(width: 8),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 55.0, horizontal: 8.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        '${items[index]['nombre']} | ${items[index]['precio']}€',
-                        style: const TextStyle(
-                          color: Colors.black,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 8),
-                      Text(
-                        items[index]['descripcion'],
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 8),
-                      ElevatedButton(
-                        onPressed: () => onTap(index),
-                        child: const Text('Añadir'),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        );
-
-
-
-        },
-        separatorBuilder: (context, index) => const Divider(),
-      ),
-    ],
-  );
-}
 
 
   // Define una función que construye una columna de listas de elementos y devuelve un widget
@@ -380,50 +385,55 @@ Widget _buildList(List<dynamic>? items, Function(int) onTap, String emptyMessage
       body: FutureBuilder<DocumentSnapshot>(
         future: bares.doc(widget.result).get(),
         builder: (context, snapshot) {
+
+          // Si estamos esperando a que se carguen los datos, mostramos un indicador de carga.
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
           }
+
+          // Si no hay datos o no existen, mostramos un mensaje de error.
           if (!snapshot.hasData || !snapshot.data!.exists) {
             return const Center(child: Text('No se han encontrado datos.'));
           }
 
           var dataBares = snapshot.data!.data() as Map<String, dynamic>;
 
-        return Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('lib/images/menu.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-          SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    dataBares['nombre'],
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
-                      letterSpacing: 1,
-                    ),
+          return Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('lib/images/menu.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
-                _buildMenu(),
-                _buildPedidos(),
-                _buildCuenta(),
-              ],
-            ),
-          ),
-          ],
-        );
+              ),
+              SingleChildScrollView(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        dataBares['nombre'],
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1,
+                        ),
+                      ),
+                    ),
+                    _buildMenu(),
+                    _buildPedidos(),
+                    _buildCuenta(),
+                  ],
+                ),
+              ),
+            ],
+          );
         },
       ),
+
       // Agregamos el botón debajo del cuerpo del scaffold.
       bottomNavigationBar: BottomAppBar(
         child: Padding(
@@ -433,6 +443,7 @@ Widget _buildList(List<dynamic>? items, Function(int) onTap, String emptyMessage
               setState(() {
                 _isCamareroEnCamino = true;
               });
+
               // Aquí podríamos agregar código para enviar una notificación al camarero.
             },
             child: Text(_isCamareroEnCamino
