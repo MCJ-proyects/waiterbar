eda8cfe7ed598a112833093293c5a9511e75e2f5
Manuel Capilla Jiménez <manucjinf4@gmail.com>
2023-06-01 21:30:29 +0200

    Inicio de app actualizado, codigo comentado

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// Importamos las dependencias necesarias para el proyecto.
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:flutter/material.dart';
@@ -6,31 +7,55 @@ import 'package:waiterbar/pages/auth_page.dart';
 import 'firebase_options.dart';
 
 void main() async {
+  // Aseguramos la inicialización de los Widgets de Flutter.
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Inicializamos Firebase con las opciones por defecto de la plataforma.
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  runApp(const WaiterBar());
+  // Ejecutamos la aplicación WaiterBar.
+  runApp(WaiterBar());
 }
 
 class WaiterBar extends StatelessWidget {
-  const WaiterBar({super.key});
+  const WaiterBar({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  // Este widget es la raíz de nuestra aplicación.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-            duration: 2000,
-            splash: "lib/images/logo.png",
+      home: Scaffold(
+        body: Center(
+          child: AnimatedSplashScreen(
+            duration: 1500,
+            splash: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "lib/images/logo.png",
+                  height: 150,
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "WaiterBar",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                ),
+              ],
+            ),
             nextScreen: AuthPage(),
             splashTransition: SplashTransition.fadeTransition,
             pageTransitionType: PageTransitionType.bottomToTop,
-            backgroundColor: Colors.blue
+            backgroundColor: Colors.blue,
+          ),
         ),
+      ),
     );
   }
 }
