b88b8662faf30682bca2c7bfd11b4bf559648ae0
Manuel Capilla Jiménez <manucjinf4@gmail.com>
2023-06-01 21:59:36 +0200

    Codigo optimizado y comentado aumentado tamaño de fuente y estilo cambiado

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,28 +1,51 @@
+// Importa el paquete de material design de flutter
 import 'package:flutter/material.dart';
 
+// Define la clase Header que extiende de StatelessWidget
 class Header extends StatelessWidget {
+  // Declara dos variables finales que serán requeridas en el constructor
   final String text;
   final String subtitle;
-  const Header({required this.text,this.subtitle="", Key? key}):super(key: key);
 
+  // Crea el constructor de la clase Header
+  const Header({
+    required this.text,
+    this.subtitle = "",
+    Key? key,
+  }) : super(key: key);
+
+  // Sobrescribe el método build para construir el widget
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // Centra el texto del título y lo estiliza con negrita y tamaño de fuente grande
+        Center(
+          child: Text(
             text,
-            style: const TextStyle(
-                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+            style: TextStyle(
+              fontSize: 30,
+              fontWeight: FontWeight.bold,
+              color: Colors.black,
+            ),
           ),
-          Text(
+        ),
+        // Centra el texto del subtítulo y lo estiliza con cursiva, tamaño de fuente mediano y color gris oscuro
+        Center(
+          child: Text(
             subtitle,
-            style: const TextStyle(
-                fontSize: 12, fontWeight: FontWeight.normal, color: Colors.grey),
+            style: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.normal,
+              fontStyle: FontStyle.italic,
+              color: const Color.fromARGB(255, 121, 117, 117),
+            ),
           ),
-        ],
+        ),
+      ],
     );
   }
 }
